868fd09e9e4bcbcfcae3c501abbb39f3c5ea8603
aruwaee <102425482+aruwaee@users.noreply.github.com>
2022-05-09 18:05:41 +0100

    added the search engine

--- src/script.js
+++ src/script.js
@@ -42,8 +42,17 @@ function displayTemp(response) {
   iconElement.setAttribute = ("alt", response.data.weather[0].description);
 }
 
-let apiKey = "6710d6c63712eab1c794b73fa7dd98bb";
-let city = "Ibadan";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "6710d6c63712eab1c794b73fa7dd98bb";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemp);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+search("Ibadan");
 
-axios.get(apiUrl).then(displayTemp);
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
